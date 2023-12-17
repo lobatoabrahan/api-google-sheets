37c632cfbf0a7b50fb5961ec60cf0e23485bb156
lobatoabrahan <lobatoabrahan@gmail.com>
2023-12-16 21:23:44 -0400

    inicio

--- api/index.py
+++ api/index.py
@@ -1,13 +1,30 @@
+from fastapi import FastAPI
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 def create_app():
-    app = Flask(__name__)
-    app.secret_key = 'oiasjdofpj4523p46j3o456'  # Reemplaza 'your-secret-key' con una clave secreta única
+    app = FastAPI()
 
-# Use the service account credentials to access the Google Sheets API
+    # Use the service account credentials to access the Google Sheets API
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('./api/keys.json', scope)
     client = gspread.authorize(creds)
+
+    # Open the Google Spreadsheet by its name
+    sheet = client.open("asistencia")
+
+    @app.get("/empleados")
+    async def get_records():
+        # Get all records from the Google Spreadsheet
+        records = sheet.worksheet("empleados").get_all_records()
+        return {"data": records}
     
-return app
+    @app.post("/empleados")
+    async def create_employee(name: str, role: str):
+        # Append a new record to the Google Spreadsheet
+        sheet.worksheet("empleados").append_row([name, role])
+        return {"message": "Employee created"}
+
+    return app
+
+app = create_app()

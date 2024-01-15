287ab4e66b7fdef5ad079febba896726f0eb6848
lobatoabrahan <83128157+lobatoabrahan@users.noreply.github.com>
2024-01-15 21:02:37 +0000

    Add form fields to crear_registro_extrusion API endpoint

--- api/produccion.py
+++ api/produccion.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Form
 from pydantic import BaseModel
 from .sheets import connect_to_sheet
 from datetime import date, time, datetime
@@ -20,21 +20,32 @@ router = APIRouter()
 sheet = connect_to_sheet("registro_produccion")
 
 @router.post("/crear_registro_extrusion")
-async def crear_registro_extrusion(registro_extrusion: Registro_Extrusion):
+async def crear_registro_extrusion(
+    fecha: datetime = Form(...),
+    turno: str = Form(...),
+    operador: str = Form(...),
+    extrusora: str = Form(...),
+    lote: str = Form(...),
+    numero_bobina: str = Form(...),
+    hora: datetime = Form(...),
+    kg: float = Form(...),
+    empates: int = Form(...),
+    estatus: str = Form(...)
+):
     try:
         worksheet = sheet.worksheet("productos")
         last_row = len(worksheet.get_all_values()) + 1
-        worksheet.update(f"C{last_row}", registro_extrusion.fecha.strftime("%d/%m/%Y"))
-        worksheet.update(f"D{last_row}", registro_extrusion.turno)
-        worksheet.update(f"E{last_row}", registro_extrusion.operador)
-        worksheet.update(f"F{last_row}", registro_extrusion.extrusora)
-        worksheet.update(f"G{last_row}", registro_extrusion.lote)
-        worksheet.update(f"J{last_row}", registro_extrusion.numero_bobina)
-        worksheet.update(f"K{last_row}", registro_extrusion.hora.strftime("%H:%M"))
-        worksheet.update(f"L{last_row}", registro_extrusion.kg)
-        worksheet.update(f"M{last_row}", registro_extrusion.empates)
-        worksheet.update(f"N{last_row}", registro_extrusion.estatus)
+        worksheet.update(f"C{last_row}", fecha.strftime("%d/%m/%Y"))
+        worksheet.update(f"D{last_row}", turno)
+        worksheet.update(f"E{last_row}", operador)
+        worksheet.update(f"F{last_row}", extrusora)
+        worksheet.update(f"G{last_row}", lote)
+        worksheet.update(f"J{last_row}", numero_bobina)
+        worksheet.update(f"K{last_row}", hora.strftime("%H:%M"))
+        worksheet.update(f"L{last_row}", kg)
+        worksheet.update(f"M{last_row}", empates)
+        worksheet.update(f"N{last_row}", estatus)
         
-        return {"message": "Materia prima creada exitosamente", "registro_extrusion": registro_extrusion}
+        return {"message": "Materia prima creada exitosamente"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
